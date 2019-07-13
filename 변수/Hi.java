dd3678594ab364bdbe957bdc3bcf6dbb81889d41
MINJOOOONG <52848040+MINJOOOONG@users.noreply.github.com>
2019-07-13 14:44:38 +0900

    Add files via upload

---
+++ 변수/Hi.java
@@ -0,0 +1,17 @@
+package 변수;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		//ctrl + d = 한줄삭제
+		
+		//이름
+		System.out.println("\n\n저는 서민주입니다.");
+		//소속
+		System.out.println("저는 전자공학과 1학년 입니다.");
+		//나이
+		System.out.println("저는 99년생 입니다.");
+		
+	}
+
+}
